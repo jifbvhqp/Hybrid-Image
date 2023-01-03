6e7d18488933f0d71de1f3190bfd35fef0321de8
品捷 黃 <jifbvhqp@gmail.com>
2023-01-03 23:39:55 +0800

    change image

--- main.py
+++ main.py
@@ -4,25 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from module import Resize,Transform,Fourier,Inv_Fourier,FilterMatrix,idealHighLowPassfilter,Dot,HybridImage,ShowImg
 
-picture_name1 = '3_cat.bmp'
-picture_name2 = '3_dog.bmp'
+picture_name1 = '4_einstein.bmp'
+picture_name2 = '4_marilyn.bmp'
 
 img1 = cv2.imread('data/'+ picture_name1 , cv2.IMREAD_COLOR)
 img2 = cv2.imread('data/'+ picture_name2 , cv2.IMREAD_COLOR)
 Hybrid_Image = HybridImage(img1,img2,25,10)
 ShowImg(Hybrid_Image)
-#cv2.imwrite('./hw2_data/task1and2_hybrid_pyramid/'+'output.jpg', Hybrid_Image)
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+cv2.imwrite('data/'+'output.jpg', Hybrid_Image)
